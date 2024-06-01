ef647fa83a8bcc4f9ca647b5d15f4fa12b26dd22
sean grogan 2 <seangroganwebdevelopment@gmail.com>
2024-06-01 10:23:46 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    let navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
